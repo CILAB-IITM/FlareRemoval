ceb1a14a354e1eeaee493a9e35626c4e1caf0e5f
saisritejakuppa <saisritejakuppa@gmail.com>
2024-03-03 12:59:31 +0530

    added download for datasets

--- dataset.sh
+++ dataset.sh
@@ -1,4 +1,7 @@
 mkdir datasets
 cd datasets && gdown 1PPXWxn7gYvqwHX301SuWmjI7IUUtqxab
 cd datasets && gdown 1GNFGWfUbgXfELx5fZtjTjU2qqWnEa-Lr
+
+# competition dataset
 cd datasets && gdown 17l2VSxrdQ-PKlkZvzD6qvODgcKtlnlVr
+cd datasets && gdown 1JTtQzsF6stWr1M0tuTez-zPBgkQ4BPVr
